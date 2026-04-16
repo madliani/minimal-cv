16eb4a6aa878bda0d092fdf754fe841c1dd6375d
madliani <madliani@hotmail.com>
2026-04-16 16:57:59 +0400

    fix: delete comments from the astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from '@astrojs/mdx'
 import expressiveCode from 'astro-expressive-code'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     expressiveCode({
