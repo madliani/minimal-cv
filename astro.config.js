54dbb1c43e02ac52ec7c54b8cc2c753e302a157f
madliani <unixsocket@hotmail.com>
2026-05-10 00:11:00 +0400

    fix: fix the astro config

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx"
 import { defineConfig } from "astro/config"
 
+/** @type {import("astro").AstroUserConfig} */
 export default defineConfig({
     integrations: [mdx()],
     site: "https://madliani-cv.vercel.app/"
