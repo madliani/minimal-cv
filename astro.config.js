1b85f91f85b9c1507e4939db5d872bdc75b310c1
madliani <unixsocket@hotmail.com>
2026-05-10 16:31:38 +0400

    feat: add @astro/sitemap

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,8 @@
+import sitemapIntegration from "@astrojs/sitemap"
 import { defineConfig } from "astro/config"
 
 /** @type {import("astro").AstroUserConfig} */
 export default defineConfig({
-    integrations: [],
+    integrations: [sitemapIntegration()],
     site: "https://madliani-cv.vercel.app/"
 })
